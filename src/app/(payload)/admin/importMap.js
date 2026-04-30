27a3622616648c08cd8ec5a9fab4c093f5a83a7f
ashton-aprnt <ashton.bryce@apparent.com.au>
2026-04-30 14:09:10 +1000

    Add Google login button to Payload import map
    - Register `@/components/payload/GoogleLoginButton` in the generated import map
    - Allow Payload admin to resolve the new default component

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -24,6 +24,7 @@ import { UploadFeatureClient as UploadFeatureClient_e70f5e05f09f93e00b997edb1ef0
 import { HorizontalRuleFeatureClient as HorizontalRuleFeatureClient_e70f5e05f09f93e00b997edb1ef0c864 } from '@payloadcms/richtext-lexical/client'
 import { SlugField as SlugField_2b8867833a34864a02ddf429b0728a40 } from '@payloadcms/next/client'
 import { CollectionCards as CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1 } from '@payloadcms/next/rsc'
+import GoogleLoginButton_b57974e7fcf897e95b6b064fd781817b from '@/components/payload/GoogleLoginButton'
 
 /** @type import('payload').ImportMap */
 export const importMap = {
@@ -52,5 +53,6 @@ export const importMap = {
   "@payloadcms/richtext-lexical/client#UploadFeatureClient": UploadFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   "@payloadcms/richtext-lexical/client#HorizontalRuleFeatureClient": HorizontalRuleFeatureClient_e70f5e05f09f93e00b997edb1ef0c864,
   "@payloadcms/next/client#SlugField": SlugField_2b8867833a34864a02ddf429b0728a40,
-  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1
+  "@payloadcms/next/rsc#CollectionCards": CollectionCards_f9c02e79a4aed9a3924487c0cd4cafb1,
+  "@/components/payload/GoogleLoginButton#default": GoogleLoginButton_b57974e7fcf897e95b6b064fd781817b
 }
